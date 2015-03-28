97fcbfcd43de1e3737ff8b075d80d80447babe5f
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2015-03-28 13:57:54 +0000

    Updates

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ var express = require('express');
 var app = express();
 app.use(cors());
 app.use(express.static('public'));
+
+/*app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/public/index.html');
+});*/
+
+app.listen(8000);
