dd9146912e17c0f5219792e4f1436b23ca5ce478
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2015-03-29 07:21:17 +0100

    Updates

--- routes.js
+++ routes.js
@@ -88,7 +88,7 @@ router.post('/analyse', ensureAuthenticated, function(req, res){
       var FB = require('fb');
       FB.setAccessToken(user.token);
 
-      FB.api('/'+req.body.id,'GET',function(r) {
+      FB.api('/'+req.body.id,'GET',{limit:1000}, function(r) {
 
         if(!r.messages) {
           res.send([]);
