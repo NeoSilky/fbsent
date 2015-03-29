28c4b1287b91946afc1116bff0bf060a66bc16de
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2015-03-29 09:04:41 +0100

    Updates

--- routes.js
+++ routes.js
@@ -103,6 +103,7 @@ router.post('/analyse', ensureAuthenticated, function(req, res){
       var processing = 0, done = 0;
 
         function parseData(r) {
+            console.log(data.length);
             console.log(r);
             for (var i = 0; i < r.messages.data.length; i++) {
                 count++;
@@ -128,8 +129,6 @@ router.post('/analyse', ensureAuthenticated, function(req, res){
                 res.send([["Date", "Score"]].concat(data));
             }
         }
-
-        console.log(resp);
 
         parseData(resp);
     });   
